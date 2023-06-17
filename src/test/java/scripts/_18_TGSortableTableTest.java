4ff90989d57e731c2c0c0ccd51e44aa6e19d588b
PaulKim098 <paul.hkim@outlook.com>
2023-06-17 16:45:03 -0500

    Added new changes scripts utilities and xml files

---
+++ src/test/java/scripts/_18_TGSortableTableTest.java
@@ -0,0 +1,48 @@
+package scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.TableHandler;
+import utils.Waiter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _18_TGSortableTableTest extends GlobalBase{
+    @BeforeMethod
+    public void setPage() {
+        driver.get("https://techglobal-training.com/frontend");
+        driver.findElement(By.id("card-11")).click();
+    }
+
+    /**
+     * Go to https://techglobal-training.com/frontend/
+     * Click on the "Sortable Tables" card
+     * Click on the Quantity sort button to toggle Ascending Order
+     * Verify that the Quantity column of the table is sorted in ascending order
+     */
+
+    @Test
+    public void validateSortAscByQuantity(){
+        WebElement ascByQuantity = driver.findElement(By.id("quantity_asc"));
+        ascByQuantity.click();
+
+        List<WebElement> quantityColumnElements = TableHandler.getTableColumn(1);
+        List<Integer> quantityColumn = new ArrayList<>();
+
+        for(WebElement element: quantityColumnElements){
+            quantityColumn.add(Integer.parseInt(element.getText()));
+        }
+
+        Waiter.pause(2);
+
+        for (int i = 1; i < quantityColumn.size(); i++) {
+            System.out.println(quantityColumn.get(i));
+            Assert.assertTrue(quantityColumn.get(i) >= quantityColumn.get(i-1));
+        }
+
+    }
+}
